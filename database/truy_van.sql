5f545d5be826b82478a262adba500cfdb7e507d4
PLVBao <vanbao11b5@gmail.com>
2026-08-24 23:08:20 +0700

    Update truy_van.sql

--- database/truy_van.sql
+++ database/truy_van.sql
@@ -1,39 +1,75 @@
-﻿USE QuanLyVatTuXayDung;
-GO
+﻿use QuanLyVatTuXayDung;
+go
 
---EXEC sp_ChuyenKho 
---    @MaPCK = 'PCK03', 
---    @MaKhoNguon = 'KHO01', 
---    @MaKhoDich = 'KHO01',
---    @MaVT = 'VT01', 
---    @SoLuongChuyen = 5, 
---    @NguoiLap = 'TK02';
+-- 1. Cảnh báo vật tư dưới mức tồn tối thiểu
+select 
+    k.MaKho,
+    k.TenKho,
+    vt.MaVT,
+    vt.TenVT,
+    vt.DonViTinh,
+    tk.SoLuongTon,
+    vt.TonToiThieu,
+    (vt.TonToiThieu - tk.SoLuongTon) as SoLuongCanNhapThem
+from TON_KHO tk
+join VAT_TU vt on tk.MaVT = vt.MaVT
+join KHO k on tk.MaKho = k.MaKho
+where tk.SoLuongTon < vt.TonToiThieu;
+go
 
---EXEC sp_ChuyenKho 
---	@MaPCK = 'PCK_TEST2',
---	@MaKhoNguon = 'KHO01',
---	@MaKhoDich = 'KHO02', 
---	@MaVT = 'VT01',
---	@SoLuongChuyen = -10, 
---	@NguoiLap = 'TK02';
+-- 2. Tra cứu tồn kho chi tiết theo từng kho và loại vật tư
+select 
+    k.MaKho,
+    k.TenKho,
+    lvt.TenLoai,
+    vt.MaVT,
+    vt.TenVT,
+    vt.DonViTinh,
+    tk.SoLuongTon
+from TON_KHO tk
+join VAT_TU vt on tk.MaVT = vt.MaVT
+join LOAI_VAT_TU lvt on vt.MaLoai = lvt.MaLoai
+join KHO k on tk.MaKho = k.MaKho
+order by k.MaKho, lvt.TenLoai;
+go
 
---EXEC sp_ChuyenKho 
---    @MaPCK = 'PCK02', 
---    @MaKhoNguon = 'KHO01', 
---    @MaKhoDich = 'KHO02', 
---    @MaVT = 'VT01', 
---    @SoLuongChuyen = 20, 
---    @NguoiLap = 'TK02';
+-- 3. Lịch sử xuất cấp vật tư cho các công trình
+select 
+    ct.MaCT,
+    ct.TenCT,
+    px.MaPX,
+    px.NgayXuat,
+    k.TenKho as KhoXuat,
+    vt.TenVT,
+    ctx.SoLuong,
+    vt.DonViTinh,
+    tk.HoTen as NguoiLapPhieu
+from PHIEU_XUAT px
+join CT_PHIEU_XUAT ctx on px.MaPX = ctx.MaPX
+join CONG_TRINH ct on px.MaCT = ct.MaCT
+join KHO k on px.MaKho = k.MaKho
+join VAT_TU vt on ctx.MaVT = vt.MaVT
+join TAI_KHOAN tk on px.NguoiLap = tk.MaTK
+order by px.NgayXuat desc;
+go
 
-
---SELECT * FROM TON_KHO WHERE MaVT = 'VT01';
---EXEC sp_ChuyenKho 
---    @MaPCK = 'PCK_TC2', 
---    @MaKhoNguon = 'KHO01', 
---    @MaKhoDich = 'KHO02', 
---    @MaVT = 'VT01', 
---    @SoLuongChuyen = 20, 
---    @NguoiLap = 'TK02';	
---SELECT * FROM TON_KHO WHERE MaVT = 'VT01';
---SELECT * FROM PHIEU_CHUYEN_KHO WHERE MaPCK = 'PCK_TC2';
---SELECT * FROM CT_CHUYEN_KHO WHERE MaPCK = 'PCK_TC2';
+-- 4. View tổng hợp Nhập - Xuất - Tồn
+create or alter view vw_TongHopNhapXuatTon
+as
+select 
+    vt.MaVT,
+    vt.TenVT,
+    vt.DonViTinh,
+    coalesce(sum(tk.SoLuongTon), 0) as TongTonHienTai,
+    coalesce((select sum(ctn.SoLuong) 
+              from CT_PHIEU_NHAP ctn 
+              join PHIEU_NHAP pn on ctn.MaPN = pn.MaPN 
+              where ctn.MaVT = vt.MaVT and pn.TrangThai = 'HOAN_THANH'), 0) as TongDaNhap,
+    coalesce((select sum(ctx.SoLuong) 
+              from CT_PHIEU_XUAT ctx 
+              join PHIEU_XUAT px on ctx.MaPX = px.MaPX 
+              where ctx.MaVT = vt.MaVT and px.TrangThai = 'HOAN_THANH'), 0) as TongDaXuat
+from VAT_TU vt
+left join TON_KHO tk on vt.MaVT = tk.MaVT
+group by vt.MaVT, vt.TenVT, vt.DonViTinh;
+go
