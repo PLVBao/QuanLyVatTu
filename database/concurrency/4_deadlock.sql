71709f9b9ffda4471451ac5eabbbf83719340b63
trawm06 <tramtt2444@ut.edu.vn>
2026-08-29 11:32:17 +0700

    Update 4_deadlock.sql

--- database/concurrency/4_deadlock.sql
+++ database/concurrency/4_deadlock.sql
@@ -12,42 +12,50 @@ GO
 
 
 -- SESSION 1: Chuyển VT01 từ KHO01 sang KHO02
-
 BEGIN TRANSACTION;
+
+PRINT N'[SESSION 1] Bat dau chuyen kho: KHO01 -> KHO02, VT01, SL = 5';
 
 UPDATE TON_KHO
 SET SoLuongTon = SoLuongTon - 5
-WHERE MaKho = 'KHO01' AND MaVT = 'VT01';
+WHERE MaKho = 'KHO01'
+  AND MaVT = 'VT01';
 
+PRINT N'[SESSION 1] Da tru kho nguon KHO01. Dang giu khoa...';
 WAITFOR DELAY '00:00:05';
+PRINT N'[SESSION 1] Chuan bi cong kho dich KHO02...';
 
 UPDATE TON_KHO
 SET SoLuongTon = SoLuongTon + 5
-WHERE MaKho = 'KHO02' AND MaVT = 'VT01';
+WHERE MaKho = 'KHO02'
+  AND MaVT = 'VT01';
+
+PRINT N'[SESSION 1] Thanh cong, tien hanh COMMIT.';
 
 COMMIT TRANSACTION;
-
-PRINT N'Session 1 đã chuyển kho thành công!';
-
 
 -- SESSION 2: Chuyển VT01 từ KHO02 sang KHO01
-
 BEGIN TRANSACTION;
 
-UPDATE TON_KHO
-SET SoLuongTon = SoLuongTon - 5
-WHERE MaKho = 'KHO02' AND MaVT = 'VT01';
+PRINT N'[SESSION 2] Bat dau chuyen kho: KHO02 -> KHO01, VT01, SL = 2';
 
+UPDATE TON_KHO
+SET SoLuongTon = SoLuongTon - 2
+WHERE MaKho = 'KHO02'
+  AND MaVT = 'VT01';
+
+PRINT N'[SESSION 2] Da tru kho nguon KHO02. Dang giu khoa...';
 WAITFOR DELAY '00:00:05';
+PRINT N'[SESSION 2] Chuan bi cong kho dich KHO01...';
 
 UPDATE TON_KHO
-SET SoLuongTon = SoLuongTon + 5
-WHERE MaKho = 'KHO01' AND MaVT = 'VT01';
+SET SoLuongTon = SoLuongTon + 2
+WHERE MaKho = 'KHO01'
+  AND MaVT = 'VT01';
+
+PRINT N'[SESSION 2] Thanh cong, tien hanh COMMIT.';
 
 COMMIT TRANSACTION;
-
-PRINT N'Session 2 đã chuyển kho thành công!';
-
 
 -- Kết quả mong đợi:
 -- Một trong hai Session bị lỗi Msg 1205 - Deadlock Victim.
@@ -59,35 +67,52 @@ PRINT N'Session 2 đã chuyển kho thành công!';
 
 
 -- SESSION 1: Chuyển VT01 từ KHO01 sang KHO02
-
 BEGIN TRANSACTION;
 
+PRINT N'[LOCK ORDERING - SESSION 1] Bat dau chuyen KHO01 -> KHO02';
+
+-- Luôn khóa KHO01 trước
 UPDATE TON_KHO
 SET SoLuongTon = SoLuongTon - 5
-WHERE MaKho = 'KHO01' AND MaVT = 'VT01';
+WHERE MaKho = 'KHO01'
+  AND MaVT = 'VT01';
 
+PRINT N'[SESSION 1] Da khoa KHO01. Dang giu khoa 5 giay...';
+WAITFOR DELAY '00:00:05';
+
+-- Sau đó mới khóa KHO02
 UPDATE TON_KHO
 SET SoLuongTon = SoLuongTon + 5
-WHERE MaKho = 'KHO02' AND MaVT = 'VT01';
+WHERE MaKho = 'KHO02'
+  AND MaVT = 'VT01';
+
+PRINT N'[SESSION 1] Da cap nhat KHO02.';
+PRINT N'[SESSION 1] COMMIT thanh cong.';
 
 COMMIT TRANSACTION;
-
-PRINT N'Session 1 đã chuyển kho thành công với Lock Ordering!';
-
 
 -- SESSION 2: Chuyển VT01 từ KHO02 sang KHO01
--- Vẫn khóa KHO01 trước KHO02
-
 BEGIN TRANSACTION;
 
-UPDATE TON_KHO
-SET SoLuongTon = SoLuongTon + 5
-WHERE MaKho = 'KHO01' AND MaVT = 'VT01';
+PRINT N'[LOCK ORDERING - SESSION 2] Bat dau chuyen KHO02 -> KHO01';
 
+-- Dù chuyển từ KHO02 về KHO01,
+-- vẫn phải khóa KHO01 trước
 UPDATE TON_KHO
-SET SoLuongTon = SoLuongTon - 5
-WHERE MaKho = 'KHO02' AND MaVT = 'VT01';
+SET SoLuongTon = SoLuongTon + 2
+WHERE MaKho = 'KHO01'
+  AND MaVT = 'VT01';
+
+PRINT N'[SESSION 2] Da khoa KHO01. Dang giu khoa 5 giay...';
+WAITFOR DELAY '00:00:05';
+
+-- Sau đó mới khóa KHO02
+UPDATE TON_KHO
+SET SoLuongTon = SoLuongTon - 2
+WHERE MaKho = 'KHO02'
+  AND MaVT = 'VT01';
+
+PRINT N'[SESSION 2] Da cap nhat KHO02.';
+PRINT N'[SESSION 2] COMMIT thanh cong.';
 
 COMMIT TRANSACTION;
-
-PRINT N'Session 2 đã chuyển kho thành công với Lock Ordering!';
